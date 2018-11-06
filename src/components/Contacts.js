21ecdd457350382286d1e2036bd6896e67b70925
Jan Gana <jan.gana@hiring-hub.com>
2018-11-06 00:24:14 +0000

    Context API

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,44 +1,24 @@
 import React, { Component } from 'react';
 import Contact from './Contact';
+import { Consumer } from './../context';
 
 class Contacts extends Component {
-  state = {
-    contacts: [
-      {
-        id: 1,
-        name: 'John Doe',
-        email: 'john.doe@gmail.com',
-        phone: '555-555-555'
-      },
-      {
-        id: 2,
-        name: 'Jessica Doe',
-        email: 'jessica.doe@gmail.com',
-        phone: '444-444-444'
-      }
-    ]
-  }
-
-  deleteContact = (id) => {
-    const { contacts } = this.state;
-    // filter out clicked contact
-    const newContacts = contacts.filter(contact => contact.id !== id);
-    // set new state
-    this.setState({ contacts: newContacts });
-  }
-
   render() {
-    const { contacts } = this.state;
-
-    return (
-      <React.Fragment>
-        {contacts.map(contact => (
-          <Contact  key={contact.id}
-                    contact={contact}
-                    deleteClickHandler={this.deleteContact.bind(this, contact.id)}
-          />
-        ))}
-      </React.Fragment>
+    return(
+      <Consumer>
+        {value => {
+          const contacts = value.contacts;
+          return(
+            <React.Fragment>
+              {contacts.map(contact => (
+                <Contact  key={contact.id}
+                          contact={contact}
+                />
+              ))}
+            </React.Fragment>
+          )
+        }}
+      </Consumer>
     )
   }
 }
