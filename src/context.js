04f1040b6e4c85ec9812f214baa84b03ec1278b3
Jan Gana <jan.gana@hiring-hub.com>
2018-11-11 19:21:37 +0000

    Edit Contact

--- src/context.js
+++ src/context.js
@@ -8,13 +8,25 @@ const reducer = (state, action) => {
     case 'DELETE_CONTACT':
       return {
         ...state,
-        contacts: state.contacts.filter(contact => contact.id !== action.payload)
+        contacts: state.contacts.filter(
+          contact => contact.id !== action.payload
+        )
       };
     case 'ADD_CONTACT':
       return {
         ...state,
         contacts: [action.payload, ...state.contacts]
-      }
+      };
+    case 'UPDATE_CONTACT':
+      return {
+        ...state,
+        contacts: state.contacts.map(
+          contact =>
+            contact.id === action.payload.id
+              ? (contact = action.payload)
+              : contact
+        )
+      };
     default:
       return state;
   }
