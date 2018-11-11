16bd0f7360c2d3720de233adcf171b080810da31
Jan Gana <jan.gana@hiring-hub.com>
2018-11-11 02:02:32 +0000

    Form validation

--- src/components/layout/TextInputGroup.js
+++ src/components/layout/TextInputGroup.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
 const TextInputGroup = ({
   name,
@@ -7,18 +8,22 @@ const TextInputGroup = ({
   value,
   placeholder,
   type,
-  onChange
+  onChange,
+  error
 }) => {
   return (
     <div className="form-group">
       <label htmlFor={name}>{label}</label>
       <input  type={type} 
-              className="form-control form-control-lg"
+              className={classnames("form-control form-control-lg", {
+                "is-invalid": error
+              })}
               placeholder={placeholder}
               name={name}
               value={value}
               onChange={onChange}
       />
+      {error && <div className="invalid-feedback">{error}</div>}
     </div>
   )
 }
@@ -29,7 +34,8 @@ TextInputGroup.propTypes ={
   value: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
   type: PropTypes.string.isRequired,
-  onChange: PropTypes.func.isRequired
+  onChange: PropTypes.func.isRequired,
+  error: PropTypes.string.isRequired
 }
 
 TextInputGroup.defaultProps = {
