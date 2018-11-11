54b6aa2505b7c98739138fca5e8641ba99deca0d
Jan Gana <jan.gana@hiring-hub.com>
2018-11-11 01:30:48 +0000

    Add Contact

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Provider } from './context';
-import Header from './components/Header';
-import Contacts from './components/Contacts';
+import Header from './components/layout/Header';
+import AddContact from './components/contacts/AddContact';
+import Contacts from './components/contacts/Contacts';
 import  'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
@@ -12,6 +13,7 @@ class App extends Component {
         <div className="App">
           <Header branding="Contact Manager" />
           <div className="container">
+            <AddContact />
             <Contacts />
           </div>
         </div>
