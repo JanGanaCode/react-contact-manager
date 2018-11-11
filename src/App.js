977151274f58f657c2a25b5b6d4c8eb79da1c957
Jan Gana <jan.gana@hiring-hub.com>
2018-11-11 03:28:20 +0000

    React Router and 404 page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import { Provider } from './context';
 import Header from './components/layout/Header';
+import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
 import AddContact from './components/contacts/AddContact';
 import Contacts from './components/contacts/Contacts';
 import  'bootstrap/dist/css/bootstrap.min.css';
@@ -10,13 +13,19 @@ class App extends Component {
   render() {
     return (
       <Provider>
-        <div className="App">
-          <Header branding="Contact Manager" />
-          <div className="container">
-            <AddContact />
-            <Contacts />
+        <Router>
+          <div className="App">
+            <Header branding="Contact Manager" />
+            <div className="container">
+              <Switch>
+                <Route path="/" exact component={Contacts}/>
+                <Route path="/contact/add" exact component={AddContact}/>
+                <Route path="/about" exact component={About}/>
+                <Route component={NotFound}/>
+              </Switch>
+            </div>
           </div>
-        </div>
+        </Router>
       </Provider>
     );
   }
