f42da59f4c320df446cef0604075fcab1d8ad3a4
Jan Gana <jan.gana@hiring-hub.com>
2018-11-11 03:44:37 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/JanGanaCode/react-contact-manager/precache-manifest.72d1ce99057916aa9d783fd8b9a28831.js"
+  "/react-contact-manager/precache-manifest.660eb9824135452263f9c6f5a46e9679.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/JanGanaCode/react-contact-manager/index.html", {
+workbox.routing.registerNavigationRoute("/react-contact-manager/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
